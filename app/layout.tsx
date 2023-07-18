163ee47e36169df49b464e92ed34bc096a98c2e8
AnLeeJVB <le.an@jvb-corp.com>
2023-07-18 16:20:48 +0700

    #

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,7 @@ function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html>
       <body>
-        <div className='w-full h-full bg-transparent bg-fixed bg-cover bg-bg-custom'>
+        <div className='w-full h-full bg-inherit bg-fixed bg-cover bg-bg-custom'>
           <Navbar />
           {children}
           <Footer />
