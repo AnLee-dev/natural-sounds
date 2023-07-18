fa296c79ed723a9a69abc920ab7beb77afcc4f59
AnLeeJVB <le.an@jvb-corp.com>
2023-07-18 13:03:46 +0700

    fix audio

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,7 @@ module.exports = {
   theme: {
     extend: {
       backgroundImage: {
-        'my-custom-bg-class': "url('https://scontent.fhan18-1.fna.fbcdn.net/v/t39.30808-6/359352897_1400158840559577_997087678695520469_n.jpg?stp=cp6_dst-jpg&_nc_cat=105&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=-D8mltaTPCgAX-I69Go&_nc_ht=scontent.fhan18-1.fna&oh=00_AfAY_puO03TCIFNlMAo9dgeF9qdiDYhLfYPlN37xamSGHg&oe=64BAB222')",
+        'bg-custom': "url('https://scontent.fhan18-1.fna.fbcdn.net/v/t39.30808-6/359352897_1400158840559577_997087678695520469_n.jpg?stp=cp6_dst-jpg&_nc_cat=105&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=-D8mltaTPCgAX-I69Go&_nc_ht=scontent.fhan18-1.fna&oh=00_AfAY_puO03TCIFNlMAo9dgeF9qdiDYhLfYPlN37xamSGHg&oe=64BAB222')",
         'icon': "url('https://asoftmurmur.com/ea27c333ccfa1e38c8c1420700b99afa.png')",
       },
     },
